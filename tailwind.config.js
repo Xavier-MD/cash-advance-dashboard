f2de978ec2be8045275362ee4486a323bddef5ba
Xavier-MD <xavier.pmd@outlook.com>
2024-10-30 13:19:36 -0400

    Edited tailwind's default shade of blue to match bree's logo.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          50: '#edf8ff',
+          100: '#d7ecff',
+          200: '#b9e0ff',
+          300: '#88ceff',
+          400: '#50b2ff',
+          500: '#2890ff',
+          600: '#1e78ff',
+          700: '#0a59eb',
+          800: '#0f47be',
+          900: '#134095',
+        },
+      },
+    },
   },
   plugins: [],
 };
