2dc08520913121a91e84b6e194bda729ff953ba7
Xavier-MD <xavier.pmd@outlook.com>
2024-10-30 12:08:12 -0400

    Added 'Sidebar.tsx' component to improve the app's layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { AppProvider } from './context/AppContext';
+import Sidebar from './components/Sidebar'
 import Dashboard from './components/Dashboard';
 
 const App: React.FC = () => {
   return (
     <AppProvider>
-      <Dashboard />
+      <Sidebar />
     </AppProvider>
   );
 };
