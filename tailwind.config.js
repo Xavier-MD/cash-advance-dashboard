1e8b4cebb67a94da73a6ad0b08426d42f7656a0b
Xavier-MD <xavier.pmd@outlook.com>
2024-10-30 16:19:40 -0400

    Added new font family.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontFamily: {
+      afacadFlux: ['Afacad Flux'],
+    },
     extend: {
       colors: {
         blue: {
